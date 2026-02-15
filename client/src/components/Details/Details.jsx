7cb105eb5fdc54bce2d7870a3e708dc8ce62fda2
LachezarBogdanov <lachezarbogdanov22@gmail.com>
2026-02-15 18:24:57 +0200

    Fixed styling

--- client/src/components/Details/Details.jsx
+++ client/src/components/Details/Details.jsx
@@ -32,6 +32,7 @@ export default function Details() {
   
     return (
         <main>
+
           {
             _id === car._ownerId ?
             <div className={styles.editDelBtns}>
@@ -40,71 +41,72 @@ export default function Details() {
             </div>
             : ''
           }
+
         <div className={styles["date"]}>
           {formatedDate}
         </div>
-  <section className={styles["car-details"]}>
-    <div className={styles["car-heading"]}>
-      <p className={styles["car-title"]}>{car.model} {car.modifications}</p>
-      <div className={styles["price"]}>
-        <p className={styles["eur"]}>{car.price} EUR</p>
-      </div>
-    </div>
-    <div className={styles["car-info"]}>
-      <p className={styles["extras"]}>
-        {car.compartment},
-        {" " + car.condition}, 
-        {" " + car.fuelType}, 
-        {" " + car.mileage}км, 
-        {" " + car.gears} скорости, 
-        {" " + car.power}, 
-        {" " + car.cubature}см3,
-        {" " + car.doorCount} врати,  
-        {" " + car.color},
-        {" " + car.year},
-        {" " + car.city}
-      </p>
-    </div>
-    <div className={styles["container"]}>
-      <div className={styles["description"]}>
-        <p>
-          {car.description}
-        </p>
-        <h3>Особености и Екстри</h3>
-        <p>
-          <b>Комфорт:</b> Климатроник, Кожен салон, Ел. стъкла, Ел. огледала,
-          Подгряване на седалки, Шибедах, Стерео уредба, Алуминиеви джанти,
-          Мултифункционален волан
-        </p>
-        <p>
-          <b>ADMRIAL AUTO 1</b>
-          <br />
-          Добрич
-        </p>
-      </div>
-      <div className={styles["gallery"]}>
-        <img src={mainImage} alt="Main Car Image" className={styles["main-image"]} />
-        <div className={styles["pagination"]}>
-          {car?.images?.map((_, index) => (
-             <span key={index} className={`${styles['dot']} ${mainImageIndex == index ? `${styles['active']}` : ''}`}/>
-          ))}
-        </div>
-        <div className={styles["thumbnails"]}>
-          {car?.images?.map((img, index) => 
-              <img key={index} src={img} alt="thumb1" onClick={() => handleMainImageChange(img, index)} />
-          )}
-          
-        </div>
-      </div>
-    </div>
-    <div className={styles["contacts"]}>
-      <p className={styles["contact-heading"]}>Контакт с продавача</p>
-      <button className={styles["phone-btn"]}>
-        <i className="fa-solid fa-phone" />
-        {car.phone}
-      </button>
-    </div>
-  </section>
+        <section className={styles["car-details"]}>
+          <div className={styles["car-heading"]}>
+            <p className={styles["car-title"]}>{car.model} {car.modifications}</p>
+            <div className={styles["price"]}>
+              <p className={styles["eur"]}>{car.price} EUR</p>
+            </div>
+          </div>
+          <div className={styles["car-info"]}>
+            <p className={styles["extras"]}>
+              {car.compartment},
+              {" " + car.condition}, 
+              {" " + car.fuelType}, 
+              {" " + car.mileage}км, 
+              {" " + car.gears} скорости, 
+              {" " + car.power}, 
+              {" " + car.cubature}см3,
+              {" " + car.doorCount} врати,  
+              {" " + car.color},
+              {" " + car.year},
+              {" " + car.city}
+            </p>
+          </div>
+          <div className={styles["container"]}>
+            <div className={styles["description"]}>
+              <p>
+                {car.description}
+              </p>
+              <h3>Особености и Екстри</h3>
+              <p>
+                <b>Комфорт:</b> Климатроник, Кожен салон, Ел. стъкла, Ел. огледала,
+                Подгряване на седалки, Шибедах, Стерео уредба, Алуминиеви джанти,
+                Мултифункционален волан
+              </p>
+              <p>
+                <b>ADMRIAL AUTO 1</b>
+                <br />
+                Добрич
+              </p>
+            </div>
+            <div className={styles["gallery"]}>
+              <img src={mainImage} alt="Main Car Image" className={styles["main-image"]} />
+              <div className={styles["pagination"]}>
+                {car?.images?.map((_, index) => (
+                  <span key={index} className={`${styles['dot']} ${mainImageIndex == index ? `${styles['active']}` : ''}`}/>
+                ))}
+              </div>
+              <div className={styles["thumbnails"]}>
+                {car?.images?.map((img, index) => 
+                    <img key={index} src={img} alt="thumb1" onClick={() => handleMainImageChange(img, index)} />
+                )}
+                
+              </div>
+            </div>
+          </div>
+          <div className={styles["contacts"]}>
+            <p className={styles["contact-heading"]}>Контакт с продавача</p>
+            <button className={styles["phone-btn"]}>
+              <i className="fa-solid fa-phone" />
+              {car.phone}
+            </button>
+          </div>
+        </section>
 </main>
 
     );
